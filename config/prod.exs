96e49ac4f87ccef82ec26bf4ee44d1cbc857f0f0
Marco "Efesto" Polita <polita.marco@gmail.com>
2018-11-06 16:47:31 +0100

    missing comma fixed

--- config/prod.exs
+++ config/prod.exs
@@ -16,7 +16,7 @@ config :skepticapp, Skepticapp.Endpoint,
   http: [:inet6, port: {:system, "PORT"}],
   url: [scheme: "https", host: "skepticapp.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
 # Do not print debug messages in production
